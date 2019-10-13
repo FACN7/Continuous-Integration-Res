f6ac9e0c0370a2966c34c9347dc2003f7ae289cc
omriza5 <omriza5@gmail.com>
2019-10-13 16:14:03 +0300

    add : 404 page

--- hello.js
+++ hello.js
@@ -2,10 +2,21 @@ var http = require('http');
 
 
 const hello = http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  res.end('Hello Travis!');
+  const endPoint = req.url;
+  if (endPoint === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.end('Hello Travis!');
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html'
+    });
+
+    res.end("Sorry :(")
+  }
+
+
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
